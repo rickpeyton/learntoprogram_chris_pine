5a044f1a6c1ba8727c8c0c198b62ca3f559f85b5
Rick Peyton <therickpeyton@gmail.com>
2014-04-01 17:28:38 -0500

    URI Import

--- testing_code.rb
+++ testing_code.rb
@@ -1,54 +1,8 @@
-require 'yaml'
-# Define yaml save and load methods...
-def yaml_save object, filename
-  File.open filename, 'w' do |f|
-    f.write(object.to_yaml)
-  end
+require 'net/http'
+uri = URI('http://stationerystyle.com/category/graduation-announcements.html')
+test = Net::HTTP.get(uri)
+
+filename  = 'froogle_test.txt'
+File.open filename, 'w' do |f|
+      f.write test
 end
-# Save it example
-# yaml_save email_addresses, filename
-
-def yaml_load filename
-  yaml_string = File.read filename
-
-  YAML::load yaml_string
-end
-# Load it example
-# read_array = yaml_load filename
-
-# filename  = 'all_dealers_yaml.txt'
-
-require 'CSV'
-filename1 = 'all-dealers.csv'
-filename2 = 'all_dealers_yaml.txt'
-
-some_array = []
-CSV.foreach(filename1) do |row|
-  some_array.push row
-end
-
-File.open filename2, 'w' do |f|
-  f.write "---\n"
-      some_array.each do |dealer|
-        f.write "#{dealer[0]}:\n"
-        f.write "  :fromname: #{dealer[1]}\n"
-        f.write "  :frommail: #{dealer[2]}\n"
-        f.write "  :logo: #{dealer[3]}\n"
-        f.write "  :web: #{dealer[4]}\n"
-        f.write "  :email: #{dealer[5]}\n"
-      end
-end
-
-# 0 AL0170
-# 1 The Paper Lady
-# 2 thepaperlady@et.printswell.com
-# 3 AL0170.gif
-# 4 http://thepaperlady.printswell.com/
-# 5 donna@thepaperlady.net
-
-# FL0511:
-#   :fromname: An Invitation to Grace
-#   :frommail: aninvitationtograce@et.printswell.com
-#   :logo: FL0511.gif
-#   :web: http://invitationtograce.printswell.com/
-#   :email: aninvitationtograce@gmail.com
